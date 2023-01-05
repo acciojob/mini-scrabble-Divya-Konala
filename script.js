7534dec2057a9bb4e54f8eaa6d9c7a64bd06d90a
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-05 20:51:56 +0530

    mini-scrabble-first-commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+function printLen() {
+  let input_text = document.querySelector("#evaluatedText").value;
+//   console.log(input_text);
+  document.querySelector("#letterCount").textContent = input_text.length;
+}
